f041c64546db4e61445a441f2f006d3dfb4c46ca
Joe Snell <joesnellpdx@gmail.com>
2012-11-13 18:04:22 -0800

    Big Daddy Layouts
    Signed-off-by: Joe Snell <joesnellpdx@gmail.com>

--- header.php
+++ header.php
@@ -62,7 +62,7 @@
 
 </head>
 
-<body data-spy="scroll" <?php body_class(); ?>>
+<body data-spy="scroll" <?php body_class('body-base'); ?>>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
