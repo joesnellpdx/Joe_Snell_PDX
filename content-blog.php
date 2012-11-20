179c4a2dffbc8c138af9789616053a1f52e76adf
Joe Snell <joesnellpdx@gmail.com>
2012-11-20 14:44:48 -0800

    Blog Content Loop & 'Code' Text
    Added styling to:
    blog posts
    blog loop
    <code>
    
    Signed-off-by: Joe Snell <joesnellpdx@gmail.com>

--- content-blog.php
+++ content-blog.php
@@ -7,6 +7,7 @@
 
 <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'joesnellpdx' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php the_post_thumbnail('full', array('class' => 'thumbnail')); ?>
 		<header class="entry-header">
 			<h2 class="entry-title"><?php the_title(); ?></h2>
 		</header><!-- .entry-header -->
